9730f0baf9eedcd96ef7fa113e73cddb67c385de
gustavts <gustavojuneo@outlook.com>
2019-08-04 02:45:00 -0300

    Adicona Procfile

--- src/models/Post.js
+++ src/models/Post.js
@@ -23,14 +23,24 @@ PostSchema.pre('save', function() {
   }
 });
 
-PostSchema.pre('remove', function() {
-  if (process.env.STORAGE_TYPE === 's3') {
-    return s3.deleteObject({
-      Bucket: process.env.S3_BUCKET,
-      Key: this.key,
-    }).promise()
+PostSchema.pre("remove", function() {
+  if (process.env.STORAGE_TYPE === "s3") {
+    return s3
+      .deleteObject({
+        Bucket: process.env.BUCKET_NAME,
+        Key: this.key
+      })
+      .promise()
+      .then(response => {
+        console.log(response.status);
+      })
+      .catch(response => {
+        console.log(response.status);
+      });
   } else {
-    return promisify(fs.unlink)(path.resolve(__dirname, '..', '..', 'tmp', 'uploads', this.key));
+    return promisify(fs.unlink)(
+      path.resolve(__dirname, "..", "..", "tmp", "uploads", this.key)
+    );
   }
 });
 
